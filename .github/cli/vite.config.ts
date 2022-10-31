97593725bc2abc5332606943c408bcbeb8ca6820
Viktor Luft <viktor.luft@freiheit.com>
2022-10-31 11:18:59 +0100

    Extend GITHUB_PATH

--- .github/cli/vite.config.ts
+++ .github/cli/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         lib: {
             entry: resolve(__dirname, 'src/main.ts'),
             name: 'MyLib',
-            fileName: (format) => `index.${format}.js`,
+            fileName: (_format) => `brocolito`,
             formats: ['cjs'],
         },
         rollupOptions: {

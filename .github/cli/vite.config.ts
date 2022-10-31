5c828bdbfb0221c192213ce6522e020ff73c89fb
Viktor Luft <viktor.luft@freiheit.com>
2022-10-31 11:11:13 +0100

    Put executable into bin dir

--- .github/cli/vite.config.ts
+++ .github/cli/vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
             external: Object.keys(packageJSON.dependencies),
             output: {
                 dir: 'build',
+                banner: '#!/usr/bin/env node',
             },
         },
     },

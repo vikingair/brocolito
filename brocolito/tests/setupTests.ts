17462e07826ccb1974b63cfb5d6e80274fb4e25a
Viktor Luft <viktor.luft@freiheit.com>
2022-11-30 14:46:28 +0100

    Fix testing issue (#17)

--- brocolito/tests/setupTests.ts
+++ brocolito/tests/setupTests.ts
@@ -1,3 +1,3 @@
 import path from 'node:path';
 
-(global as any).__BROCOLITO__ = { name: 'cli', dir: path.resolve(''), version: '1.2.3' };
+(global as any).__BROCOLITO__ = { name: 'cli', dir: path.resolve('.'), version: '1.2.3' };

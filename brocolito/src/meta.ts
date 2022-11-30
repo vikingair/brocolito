24e5edae1f19f0912bf8e742348e05fa7bcfdaa9
Viktor Luft <viktor.luft@freiheit.com>
2022-11-30 14:43:55 +0100

    Fix testing issue

--- brocolito/src/meta.ts
+++ brocolito/src/meta.ts
@@ -1,5 +1,9 @@
+import path from 'node:path';
+
 // we don't declare this type, because it should not be accessed by lib users
 // see bin/build.mjs
-const { name, dir, version } = (global as any).__BROCOLITO__; // eslint-disable-line @typescript-eslint/no-explicit-any
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const injected = (global as any).__BROCOLITO__ || { dir: path.resolve('.'), name: 'dummy', version: 'dummy' };
+const { name, dir, version } = injected;
 
 export const Meta: { name: string; dir: string; version: string } = { name, dir, version };

bb509bcbccfaa2bafa51c31360a870a28e73ada5
Viktor Luft <viktor.luft@freiheit.com>
2023-10-28 15:14:37 +0200

    Improve error messages for passed args

--- brocolito/src/parse.ts
+++ brocolito/src/parse.ts
@@ -8,7 +8,7 @@ import { Meta } from "./meta";
 
 const _findByNameOrAlias = (
   name: string,
-  commands: Record<string, Command>
+  commands: Record<string, Command>,
 ): Command | undefined => {
   if (name in commands) return commands[name];
   return Object.values(commands).find(({ alias }) => alias === name);
@@ -16,40 +16,63 @@ const _findByNameOrAlias = (
 
 const _findSubcommand = (
   command: Command,
-  subcommandsOrArgs: string[]
+  subcommandsOrArgs: string[],
 ): [command: Command, args: string[]] => {
   if (!subcommandsOrArgs.length) return [command, subcommandsOrArgs];
   const subcommand = _findByNameOrAlias(
     subcommandsOrArgs[0],
-    command.subcommands
+    command.subcommands,
   );
-  if (!subcommand) return [command, subcommandsOrArgs];
+  if (!subcommand) {
+    if (Object.keys(command.subcommands).length && subcommandsOrArgs.length) {
+      Help.show(command);
+      return Utils.complainAndExit(
+        `Unknown subcommand ${Utils.pc.yellow(
+          subcommandsOrArgs[0],
+        )} specified.`,
+      );
+    }
+    return [command, subcommandsOrArgs];
+  }
   return _findSubcommand(subcommand, subcommandsOrArgs.slice(1));
 };
 
 export const findCommand = (
-  commands: string[]
-): [command: Command, args: string[]] | string => {
+  commands: string[],
+): [command: Command, args: string[]] => {
   const command = _findByNameOrAlias(commands[0], State.commands);
   if (!command) {
-    return `Command "${commands[0]}" does not exist`;
+    return Utils.complainAndExit(`Command "${commands[0]}" does not exist`);
   }
   return _findSubcommand(command, commands.slice(1));
 };
 
 const _parseArgs = (
   command: Command,
-  args: string[]
+  args: string[],
 ): Record<string, string | string[] | undefined> => {
-  const errorDetails = `Expected ${command.args.length} arguments, but was invoked with ${args.length}`;
+  const throwError = (reason: string, remainingArgs?: string[]) => {
+    Help.show(command);
+    return Utils.complainAndExit(
+      `${reason}: Expected ${
+        command.args.length
+      } arguments, but was invoked with ${args.length}.${
+        remainingArgs
+          ? `
+The following arguments could not be processed: ${Utils.pc.yellow(
+              remainingArgs.join(" "),
+            )}`
+          : ""
+      }`,
+    );
+  };
+
   let usedArgs = [...args];
   const result = Object.fromEntries(
     command.args.map(({ usage, name }): [string, string | string[]] => {
       const multi = /\.{3}>$/.test(usage);
       if (!usedArgs.length) {
-        return Utils.complainAndExit(
-          "Too few arguments given: " + errorDetails
-        );
+        return throwError("Too few arguments given");
       } else if (!multi) {
         const arg = usedArgs.shift();
         return [name, arg as string];
@@ -58,17 +81,15 @@ const _parseArgs = (
         usedArgs = [];
         return [name, copy];
       }
-    })
+    }),
   );
-  if (usedArgs.length) {
-    return Utils.complainAndExit("Too many arguments given: " + errorDetails);
-  }
+  if (usedArgs.length) return throwError("Too many arguments given", usedArgs);
   return result;
 };
 
 const _parseOptions = (
   command: Command,
-  minimistOptions: Omit<minimist.ParsedArgs, "_">
+  minimistOptions: Omit<minimist.ParsedArgs, "_">,
 ): Record<string, string | boolean | undefined> => {
   const opts: Record<string, string | boolean | undefined> = {};
   Object.entries(command.options as Record<string, OptionMeta>).forEach(
@@ -83,24 +104,24 @@ const _parseOptions = (
       if (type === "boolean") {
         if (typeof value === "string" && value !== "true" && value !== "false")
           Utils.complainAndExit(
-            `Invalid value "${value}" provided for flag ${prefixedName}`
+            `Invalid value "${value}" provided for flag ${prefixedName}`,
           );
         opts[camelName] = value === true || value === "true";
       } else {
         if (Array.isArray(value))
           Utils.complainAndExit(
-            `Invalidly multiple values "${value}" were provided for flag ${prefixedName}`
+            `Invalidly multiple values "${value}" were provided for flag ${prefixedName}`,
           );
         opts[camelName] = value;
       }
-    }
+    },
   );
   const remainingArgs = Object.keys(minimistOptions);
   if (remainingArgs.length) {
     Utils.complainAndExit(
       `Unrecognized options were used: ${remainingArgs
         .map((name) => "--" + name)
-        .join(", ")}`
+        .join(", ")}`,
     );
   }
   return opts;
@@ -108,7 +129,7 @@ const _parseOptions = (
 
 export const parse = async (argv = process.argv): Promise<unknown> => {
   const { _: minimistArgs, ...minimistOpts } = minimist(
-    argv.slice(2).filter(Boolean)
+    argv.slice(2).filter(Boolean),
   );
   const firstArg = minimistArgs[0];
   if (firstArg === "completion") return Completion.run();
@@ -118,17 +139,14 @@ export const parse = async (argv = process.argv): Promise<unknown> => {
     if (minimistOpts.v || minimistOpts.version)
       return console.log(Meta.version);
   }
-  const foundCommand = findCommand(minimistArgs);
-  if (typeof foundCommand === "string")
-    return Utils.complainAndExit(foundCommand);
-  const [command, args] = foundCommand;
+  const [command, args] = findCommand(minimistArgs);
   if (wantsHelp) return Help.show(command);
   const parsedArgs = _parseArgs(command, args);
   const options = _parseOptions(command, minimistOpts);
   const action = command._action;
   if (!action) {
     return Utils.complainAndExit(
-      `Configuration error: No action for given command "${command.line}" specified`
+      `Configuration error: No action for given command "${command.line}" specified`,
     );
   }
   return action({ ...options, ...parsedArgs });
